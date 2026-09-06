31f804921855c89a6e5b788aea393de4b359610f
nikhil-aum <nikhilpatidar.aum@outlook.com>
2026-09-06 21:47:37 +0530

    adding some extra fields (#1)

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,7 @@ public class hello {
 
     public static void main(String[] args) {
         System.out.printf("This is testing");
+        System.out.println("My name is Ram");
+        System.out.printf("My age is 22git");
     }
 }
